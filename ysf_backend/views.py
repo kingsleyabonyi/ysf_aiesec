00779ce0d3e090c24528258a2304e4907cd7fcfa
Kingsley <kinabonyi@gmail.com>
2024-05-01 14:02:19 +0100

    seven commit

--- ysf_backend/views.py
+++ ysf_backend/views.py
@@ -10,9 +10,11 @@ class UserCreateView(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            user = serializer.save()
+            # user.set_password(serializer.data["password"])
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 
 
 
