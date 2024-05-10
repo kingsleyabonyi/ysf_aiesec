6a4b17653070e1fbbc5c972337b10ec506b69b0d
Kingsley <kinabonyi@gmail.com>
2024-05-10 12:30:04 +0100

    edited the serializer

--- ysf_backend/serializers.py
+++ ysf_backend/serializers.py
@@ -7,7 +7,7 @@ from rest_framework import serializers, validators
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email', 'phone_number', 'location', 'university', 'age_range', 'sdg_interest', 'attendance_type', 'accommodation_needed', 'academic_level', 'academic_major', 'how_hear_about_ysf', 'contact_preference', 'employment_status', 'expectations', 'shirt_size']
+        fields = ['first_name', 'last_name', 'email', 'phone_number', 'location', 'university', 'age_range', 'sdg_interest', 'attendance_type', 'academic_level', 'academic_major', 'how_hear_about_ysf', 'contact_preference', 'employment_status', 'expectations', 'shirt_size']
 
 
 
